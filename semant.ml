3ab8c7064cb9a54d07d98a80ef51f1d491e4334c
Kidus Mulu <km3533@columbia.edu>
2021-03-30 16:13:24 -0400

    added an easier/extendable interface to semantic errors

--- semant.ml
+++ semant.ml
@@ -6,6 +6,17 @@ open Sast
 
 module StringMap = Map.Make(String)
 
+(* A semantic error in cnet. It contains a message and a line number *)
+
+(* Kidus: The line number is not really implemented and is only there for
+ * forward compatibility. Right now the function semant_err takes only a string,
+ * but later it can take a line number as well
+ * *)
+exception SemanticError of string * int;;
+
+(* The function for raising a semantic error *)
+let semant_err (msg : string) =
+  raise (SemanticError(msg, -1));;
 (* Semantic checking of the AST. Returns an SAST if successful,
    throws an exception if something is wrong.
 
@@ -22,7 +33,7 @@ let check  = function
 
   let to_ast_func = function
       Fdecl(func) -> func
-    | _ -> raise (Failure ("illegal type passed to_ast_func ")) in
+    | _ -> semant_err "illegal type passed to_ast_func " in
 
   let functions = List.map  to_ast_func func_decl_list in
 
@@ -67,11 +78,10 @@ let check  = function
   let add_func map (fd: func) =
     let built_in_err = "function " ^ fd.name ^ " may not be defined"
     and dup_err = "duplicate function " ^ fd.name
-    and make_err er = raise (Failure er)
     and n = fd.name (* Name of the function *)
     in match fd with (* No duplicate functions or redefinitions of built-ins *)
-         _ when StringMap.mem n built_in_decls -> make_err built_in_err
-       | _ when StringMap.mem n map -> make_err dup_err
+         _ when StringMap.mem n built_in_decls -> semant_err built_in_err
+       | _ when StringMap.mem n map -> semant_err dup_err
        | _ ->  StringMap.add n fd map
   in
 
@@ -82,11 +92,12 @@ let check  = function
   (* Return a function from our symbol table *)
   let find_func (s : string) =
     try StringMap.find s function_decls
-    with Not_found -> raise (Failure ("unrecognized function " ^ s))
+    with Not_found -> semant_err ("unrecognized function " ^ s)
   in
 
 (* Ensure "main" is defined *)
-  let _ = find_func "main" in
+  let _ = try find_func "main"
+    with _ -> semant_err "main function not found" in
 
   let check_function func =
     (* Make sure no formals or locals are void or duplicates *)
@@ -96,7 +107,7 @@ let check  = function
     (* Raise an exception if the given rvalue type cannot be assigned to
        the given lvalue type *)
     let check_assign lvaluet rvaluet err =
-       if lvaluet = rvaluet then lvaluet else raise (Failure err)
+       if lvaluet = rvaluet then lvaluet else semant_err err
     in
 
     (* Build local symbol table of variables for this function *)
@@ -106,7 +117,7 @@ let check  = function
 
     let type_of_identifier s =
       try StringMap.find s symbols
-      with Not_found -> raise (Failure ("undeclared identifier " ^ s))
+      with Not_found -> semant_err ("undeclared identifier " ^ s)
     in
 
     (* Return a semantically-checked expression, i.e., with a type *)
@@ -127,9 +138,9 @@ let check  = function
           let (t, e') = expr e in
           let ty = match op with
             (Minus | Not) when t = Int || t = Float -> t
-          | _ -> raise (Failure ("illegal unary operator " ^
+          | _ -> semant_err ("illegal unary operator " ^
                                  string_of_uop op ^ string_of_typ t ^
-                                 " in " ^ string_of_expr ex))
+                                 " in " ^ string_of_expr ex)
           in (ty, SUnop(op, (t, e')))
       | Binop(e1, op, e2) as e ->
           let (t1, e1') = expr e1
@@ -144,17 +155,16 @@ let check  = function
           | Lt | Leq | Gt | Geq
                      when same && (t1 = Int || t1 = Float) -> Int
           | And | Or when same && t1 = Int -> Int
-          | _ -> raise (
-	      Failure ("illegal binary operator " ^
+          | _ -> semant_err ("illegal binary operator " ^
                        string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
-                       string_of_typ t2 ^ " in " ^ string_of_expr e))
+                       string_of_typ t2 ^ " in " ^ string_of_expr e)
           in (ty, SBinop((t1, e1'), op, (t2, e2')))
       | Call(fname, args) as call ->
           let fd = find_func (U.final_id_of_rid fname) in
           let param_length = List.length fd.parameters in
           if List.length args != param_length then
-            raise (Failure ("expecting " ^ string_of_int param_length ^
-                            " arguments in " ^ string_of_expr call))
+            semant_err ("expecting " ^ string_of_int param_length ^
+                            " arguments in " ^ string_of_expr call)
           else let check_call (ft, _) e =
                 let (et, e') = expr e in
                 let err = "illegal argument found " ^ string_of_typ et ^
@@ -163,13 +173,13 @@ let check  = function
           in
           let args' = List.map2 check_call fd.parameters args
           in (fd.t, SCall(fname, args'))
-      | _ -> raise (Failure ("Expression not yet implemented"))
+      | _ -> semant_err "Expression not yet implemented"
     in
 
     let check_bool_expr e =
       let (t', e') = expr e
       and err = "expected Boolean expression in " ^ string_of_expr e
-      in if t' != Int then raise (Failure err) else (t', e')
+      in if t' != Int then semant_err err else (t', e')
     in
 
     (* Return a semantically-checked statement i.e. containing sexprs *)
@@ -188,21 +198,20 @@ let check  = function
 
       | Return e -> let (t, e') = expr e in
         if t = func.t then SReturn (t, e')
-        else raise (
-	  Failure ("return gives " ^ string_of_typ t ^ " expected " ^
-		   string_of_typ func.t ^ " in " ^ string_of_expr e))
+        else semant_err ("return gives " ^ string_of_typ t ^ " expected " ^
+		   string_of_typ func.t ^ " in " ^ string_of_expr e)
 
 	    (* A block is correct if each statement is correct and nothing
 	       follows any Return statement.  Nested blocks are flattened. *)
       | Block sl ->
           let rec check_stmt_list = function
               [Return _ as s] -> [check_stmt s]
-            | Return _ :: _   -> raise (Failure "nothing may follow a return")
+            | Return _ :: _   -> semant_err "nothing may follow a return"
             | Block sl :: ss  -> check_stmt_list (sl @ ss) (* Flatten blocks *)
             | s :: ss         -> check_stmt s :: check_stmt_list ss
             | []              -> []
           in SBlock(check_stmt_list sl)
-      | _ -> raise (Failure ("Statement not yet implemented"))
+      | _ -> semant_err "Statement not yet implemented"
 
     in (* body of check_function *)
     { styp = func.t;
@@ -210,7 +219,7 @@ let check  = function
       sparameters = func.parameters;
       sbody = match check_stmt (Block func.body) with
 	SBlock(sl) -> sl
-      | _ -> raise (Failure ("internal error: block didn't become a block?"))
+        | _ -> semant_err "[COMPILER BUG] block didn't become a block?"
     }
   in    (* (globals, List.map check_function functions) *)
 
