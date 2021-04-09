24f882b9e22a88290796b347cafa80a83abcfe10
William Oseghare <osegharewilliam@gmail.com>
2021-04-09 08:52:55 -0400

    test2.c not working yet

--- tests/stdlib/test2.c
+++ tests/stdlib/test2.c
@@ -16,22 +16,22 @@ int main()
 
 	assert(strcmp(s1->data, s2->data) == 0);
 
-	// assert(strcmp(s1->data, s2->data) == cnet_strcmp(s1, s2));
+	assert(strcmp(s1->data, s2->data) == cnet_strcmp(s1, s2));
 
-	// string *s3 = cnet_new_str(" ", 1);
-	// string *s4 = cnet_str_upper(s1);
+	string *s3 = cnet_new_str(" ", 1);
+	string *s4 = cnet_str_upper(s1);
 
-	// string *s5 = cnet_strcat(s4, s3);
-	// string *s6 = cnet_strcat(s5, s2);
+	string *s5 = cnet_strcat(s4, s3);
+	string *s6 = cnet_strcat(s5, s2);
 	
-	// printf("%s\n%s\n",s5->data, s6->data);
+	printf("%s\n%s\n",s5->data, s6->data);
 
 	cnet_free(s1);
 	cnet_free(s2);
-	// cnet_free(s3);
-	// cnet_free(s4);
-	// cnet_free(s5);
-	// cnet_free(s6);
+	cnet_free(s3);
+	cnet_free(s4);
+	cnet_free(s5);
+	cnet_free(s6);
 
 	return 0;
 }
