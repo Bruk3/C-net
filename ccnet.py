34b7d05e21211adece686f098df01a7258af5369
Kidus Mulu <km3533@columbia.edu>
2021-04-18 18:37:25 -0400

    added full compilation to new ccnet toplevel

--- ccnet.py
+++ ccnet.py
@@ -3,11 +3,12 @@
 import getopt
 import sys
 import os
+import subprocess
 
 CC		= "gcc"			# C compiler
 LLC		= "llc"			# LLVM compiler
 CNET	= "cnet.native" # CNET compiler
-LIBCNET = "libcnet/*c"
+LIBCNET = "libcnet/libcnet.a"
 
 USAGEMSG  = """USAGE: {} -(t|a|s|l|b) source """.format(sys.argv[0])
 
@@ -32,15 +33,55 @@ def handle_normal(options, sourcef):
 		sys.exit(1)
 
 def handle_full(options, sourcef):
-		if 'o' in options: # user specified a file
-				pass
+		def die(ret):
+				print(ret.stderr.decode(), file=sys.stderr, end='')
+				sys.exit(ret.returncode)
 
-		#
+
+
+		# compile cnet to LLVM
+		args = ["./" + CNET, "-c", sourcef]
+		ret = subprocess.run(args, capture_output=True)
+		if (ret.returncode != 0): die(ret)
+
+		# compile LLVM to assembly
+		args = [LLC]
+		ret = subprocess.run(args, capture_output=True, input=ret.stdout)
+		if (ret.returncode != 0): die(ret)
+
+		# compile assembly to C
+		tmpfile = "/tmp/" + sourcef + ".s"
+		with open(tmpfile, "w") as tmp:
+				tmp.write(ret.stdout.decode())
+
+		args = [CC, tmpfile, LIBCNET, "-o", "/dev/stdout"]
+		ret = subprocess.run(args, capture_output=True)
+
+		if (ret.returncode != 0): die(ret)
+
+
+		outputf = ""
+		if 'o' in options: # user specified a file
+				with open(options['o'], 'wb') as outfile:
+						outfile.write(ret.stdout)
+				sys.exit(0)
+		# else:
+		# 		outputf = sourcef.split('.')
+		# 		with open(outputf, 'wb') as outfile:
+		# 				outfile.write(ret.stdout)
+		# 		sys.exit(0)
+
+
+		sys.stdout.buffer.write(ret.stdout)
+		# print(ret.stdout.decode(), end='')
+		sys.exit(0)
+
+		# print(ret)
 
 
 
 def main():
-		opts_l, source = getopt.getopt(sys.argv[1:], "taslb")
+		opts_l, source = getopt.getopt(sys.argv[1:], "taslbco:")
 		# print(opts_l, source)
 
 		options = dict()
@@ -49,11 +90,15 @@ def main():
 
 		# print(options)
 
-		if invalid_options(options, source): print(USAGEMSG); sys.exit(1);
+		if invalid_options(options, source):
+				print(USAGEMSG)
+				sys.exit(1)
 
 
-		if "b" not in options: # not a full compilation
+		if "b" not in options and len(options) > 0: # not a full compilation
 				handle_normal(options, source[0])
+		else:
+				handle_full(options, source[0])
 
 
 main();
